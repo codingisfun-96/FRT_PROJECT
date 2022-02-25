627913580492ab549661938d5a57fc32aef98e70
codingisfun-96 <saikatmaji96@outlook.com>
2022-02-25 06:00:35 +0530

    updated

--- routes/auth.js
+++ routes/auth.js
@@ -5,9 +5,7 @@ const db= require('../config/database')
 
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
-// router.post('/forgot-password', authController.forgotpassword);
-// router.get('/reset-password/:id/:token',authController.loadresetpassword);
-// router.post('/reset-password/:id/:token',authController.resetpassword);
+
 
 
 module.exports = router;
